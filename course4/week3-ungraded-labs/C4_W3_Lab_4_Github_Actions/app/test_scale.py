f1384596b52c47016520b36db5847bcd38bbea78
novacontar <50725386+novacontar@users.noreply.github.com>
2024-07-03 08:20:07 +0200

    added scaler test

---
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_scale.py
@@ -0,0 +1,10 @@
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def test_pipeline_and_scaler():
+    isPipeline = instance(clf, Pipeline)
+    assert isPipeline
+    if isPipeline:
+        firstStep = [v for v in clf.named_steps.values()][0]
+    assert isinstance(firstStep, StandardScaler)
